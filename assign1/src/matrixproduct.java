e28b96121a8366c6d38ee7f47ac3251d7d2ae03f
inessalexandre <up202103343@up.pt>
2023-03-06 11:13:04 +0000

    added java reults

--- assign1/src/matrixproduct.java
+++ assign1/src/matrixproduct.java
@@ -1,9 +1,25 @@
 import java.util.*;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class MatrixProduct{
+class MatrixProduct {
+	private static FileWriter writer;
+	private static FileWriter writer2;
+	
+	public MatrixProduct() {
+		try {
+			writer = new FileWriter("ResultsEX1.txt");
+			writer2 = new FileWriter("ResultsEX2_line.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+	 	}
+	}
 
-	public static void OnMult(int m_ar, int m_br) {
-		
+	public static void OnMult(int m_ar, int m_br) throws IOException {
+		if (writer == null) {
+			writer = new FileWriter("ResultsEX1.txt");
+		}
+		int numIteracoes = 10;
 		long Time1, Time2;
 
 		String st; // not used. keeping here just in case
@@ -22,33 +38,42 @@ public class MatrixProduct{
 			for(j=0; j<m_br; j++)
 				phb[i*m_br + j] = (double)(i+1);
 
-		Time1 = System.currentTimeMillis();
+		for (int count = 1; count <= numIteracoes; count++) {
+			Time1 = System.currentTimeMillis();
 
-		for(i=0; i<m_ar; i++){
-			for(j=0; j<m_br; j++){
-				temp = 0;
-				for( k=0; k<m_ar; k++)
-				{	
-					temp += pha[i*m_ar+k] * phb[k*m_br+j];
+			for(i=0; i<m_ar; i++){
+				for(j=0; j<m_br; j++){
+					temp = 0;
+					for( k=0; k<m_ar; k++) {   
+						temp += pha[i*m_ar+k] * phb[k*m_br+j];
+					}
+					phc[i*m_ar+j]=temp;
 				}
-				phc[i*m_ar+j]=temp;
 			}
-		}
 
-		Time2 = System.currentTimeMillis();
+			Time2 = System.currentTimeMillis();
 
-		System.out.printf("Time: %3.3f seconds\n", (double)(Time2-Time1)/1000);
-		
-		System.out.println("Result matrix: ");
-		for (i = 0; i < 1; i++) {
-			for (j = 0; j < Math.min(10, m_br); j++) {
-				System.out.print(phc[j] + " ");
+			//writer.write(String.format("Time: %3.3f seconds\n", (double)(Time2-Time1)/1000));
+			if (count==1) {
+				writer.write(String.format("Matrix size: %d x %d\n\n", m_ar, m_ar));
+				writer.write("Result matrix: \n");
+				for (i = 0; i < 1; i++) {
+					for (j = 0; j < Math.min(10, m_br); j++) {
+						writer.write(phc[j] + " ");
+					}
+				}
+				writer.write("\n\n");
 			}
+			writer.write(String.format("Time: %3.3f seconds\n", (double)(Time2-Time1)/1000));
 		}
-		System.out.println();
 	}
 
-	public static void OnMultLine(int m_ar, int m_br) {
+	public static void OnMultLine(int m_ar, int m_br) throws IOException {
+		if (writer2 == null) {
+			writer2 = new FileWriter("ResultsEX2_line.txt");
+		}
+
+		int numIteracoes = 10;
 		long Time1, Time2;
 
 		String st; // not used. keeping here just in case
@@ -71,36 +96,36 @@ public class MatrixProduct{
 			for(j=0; j<m_ar; j++)
 				phc[i*m_ar + j] = (double)0;
 
-		Time1 = System.currentTimeMillis();
+			
+		for (int count = 1; count <= numIteracoes; count++) {
+			Time1 = System.currentTimeMillis();
 
-		for(i=0; i<m_ar; i++)
-		{
-			for( k=0; k<m_ar; k++)
-			{	
-				for(j=0; j<m_br; j++)
-				{
-					phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+			for(i=0; i<m_ar; i++) {
+				for(k=0; k<m_ar; k++) {	
+					for(j=0; j<m_br; j++) {
+						phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+					}
 				}
 			}
-		}
 
-		Time2 = System.currentTimeMillis();
+			Time2 = System.currentTimeMillis();
 
-		System.out.printf("Time: %3.3f seconds\n", (double)(Time2-Time1)/1000);
-		
-		System.out.println("Result matrix: ");
-		for (i = 0; i < 1; i++) {
-			for (j = 0; j < Math.min(10, m_br); j++) {
-				System.out.print(phc[j] + " ");
+			if (count==1) {
+				writer2.write(String.format("Matrix size: %d x %d\n\n", m_ar, m_ar));
+				writer2.write("Result matrix: \n");
+				for (i = 0; i < 1; i++) {
+					for (j = 0; j < Math.min(10, m_br); j++) {
+						writer2.write(phc[j] + " ");
+					}
+				}
+				writer2.write("\n\n");
 			}
+			writer2.write(String.format("Time: %3.3f seconds\n", (double)(Time2-Time1)/1000));
 		}
-		System.out.println();
 	}
 
+	public static void main(String[] args) throws IOException {	
 
-
-	public static void main(String[] args) {	
-	
 		char c;
 		int lin, col, blockSize;
 		int op;
@@ -127,10 +152,26 @@ public class MatrixProduct{
 
 			switch (op){
 				case 1: 
+					/*OnMult(600, 600);
+					OnMult(1000, 1000);
+					OnMult(1400, 1400);
+					OnMult(1800, 1800);
+					OnMult(2200, 2200);
+					OnMult(2600, 2600); 
+					OnMult(3000, 3000); */
 					OnMult(lin, col);
+					writer.close();
 					break;
 				case 2:
-					OnMultLine(lin, col);  
+					/*OnMultLine(600, 600); 
+					OnMultLine(1000, 1000);
+					OnMultLine(1400, 1400);
+					OnMultLine(1800, 1800);
+					OnMultLine(2200, 2200);
+					OnMultLine(2600, 2600); 
+					OnMultLine(3000, 3000); */
+					OnMultLine(lin, col);
+					writer2.close();
 					break;
 			}
 		} while (op != 0);
--- assign1/src/teste.cpp
+++ assign1/src/teste.cpp
@@ -3,18 +3,20 @@
 #include <iomanip>
 #include <time.h>
 #include <cstdlib>
-#include <papi.h>
+#include <fstream>
+//#include <papi.h>
 
 using namespace std;
-
 #define SYSTEMTIME clock_t
 
+std::ofstream fileTXT("resultsCpp.txt");
+	
  
-void OnMult(int m_ar, int m_br) 
-{
-	
+void OnMult(int m_ar, int m_br)  {
+
 	SYSTEMTIME Time1, Time2;
-	
+
+	int numIteracoes = 10;
 	char st[100];
 	double temp;
 	int i, j, k;
@@ -29,8 +31,6 @@ void OnMult(int m_ar, int m_br)
 		for(j=0; j<m_ar; j++)
 			pha[i*m_ar + j] = (double)1.0;
 
-
-
 	for(i=0; i<m_br; i++)
 		for(j=0; j<m_br; j++)
 			phb[i*m_br + j] = (double)(i+1);
@@ -38,40 +38,46 @@ void OnMult(int m_ar, int m_br)
 	for(int i = 0; i < m_ar*m_br; ++i){
 		phc[i] = 0;
 	}
-    Time1 = clock();
+	
+	for (int count = 1; count <= numIteracoes; count++) {
+    
+		Time1 = clock();
 
-	for(i=0; i<m_ar; i++)
-	{	for( j=0; j<m_br; j++)
-		{	temp = 0;
-			for( k=0; k<m_ar; k++)
-			{	
-				temp += pha[i*m_ar+k] * phb[k*m_br+j];
+
+		for(i=0; i<m_ar; i++)
+		{	for( j=0; j<m_br; j++)
+			{	temp = 0;
+				for( k=0; k<m_ar; k++)
+				{	
+					temp += pha[i*m_ar+k] * phb[k*m_br+j];
+				}
+				phc[i*m_ar+j]=temp;
 			}
-			phc[i*m_ar+j]=temp;
 		}
+
+
+		Time2 = clock();
+
+		// display 10 elements of the result matrix tto verify correctness
+		fileTXT << "Result matrix: " << endl;
+		for(i=0; i<1; i++) {	
+			for(j=0; j<min(10,m_br); j++)
+				fileTXT << phc[j] << " ";
+		}
+
+		fileTXT << endl;
+		sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+		fileTXT << st;
+
+		free(pha);
+		free(phb);
+		free(phc);
 	}
-
-
-    Time2 = clock();
-	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
-	cout << st;
-
-	// display 10 elements of the result matrix tto verify correctness
-	cout << "Result matrix: " << endl;
-	for(i=0; i<1; i++)
-	{	for(j=0; j<min(10,m_br); j++)
-			cout << phc[j] << " ";
-	}
-	cout << endl;
-
-    free(pha);
-    free(phb);
-    free(phc);
+	fileTXT.close();
 }
 
 // add code here for line x line matriz multiplication
-void OnMultLine(int m_ar, int m_br)
-{
+void OnMultLine(int m_ar, int m_br) {
     	
 	SYSTEMTIME Time1, Time2;
 	
@@ -186,7 +192,7 @@ void OnMultBlock(int m_ar, int m_br, int bkSize)
 }
 
 
-
+/*
 void handle_error (int retval)
 {
   printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
@@ -205,19 +211,19 @@ void init_papi() {
             << " MINOR: " << PAPI_VERSION_MINOR(retval)
             << " REVISION: " << PAPI_VERSION_REVISION(retval) << "\n";
 }
+*/
 
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
 
 	char c;
 	int lin, col, blockSize;
 	int op;
 	
-	int EventSet = PAPI_NULL;
+	//int EventSet = PAPI_NULL;
   	long long values[2];
   	int ret;
 	
-
+/*
 	ret = PAPI_library_init( PAPI_VER_CURRENT );
 	if ( ret != PAPI_VER_CURRENT )
 		std::cout << "FAIL" << endl;
@@ -233,7 +239,7 @@ int main (int argc, char *argv[])
 
 	ret = PAPI_add_event(EventSet,PAPI_L2_DCM);
 	if (ret != PAPI_OK) cout << "ERROR: PAPI_L2_DCM" << endl;
-
+*/
 
 	op=1;
 	do {
@@ -250,8 +256,8 @@ int main (int argc, char *argv[])
 
 
 		// Start counting
-		ret = PAPI_start(EventSet);
-		if (ret != PAPI_OK) cout << "ERROR: Start PAPI" << endl;
+		//ret = PAPI_start(EventSet);
+		//if (ret != PAPI_OK) cout << "ERROR: Start PAPI" << endl;
 
 		switch (op){
 			case 1: 
@@ -267,7 +273,7 @@ int main (int argc, char *argv[])
 				break;
 
 		}
-
+/*
   		ret = PAPI_stop(EventSet, values);
   		if (ret != PAPI_OK) cout << "ERROR: Stop PAPI" << endl;
   		printf("L1 DCM: %lld \n",values[0]);
@@ -277,10 +283,10 @@ int main (int argc, char *argv[])
 		if ( ret != PAPI_OK )
 			std::cout << "FAIL reset" << endl; 
 
-
+*/
 
 	}while (op != 0);
-
+/*
 	ret = PAPI_remove_event( EventSet, PAPI_L1_DCM );
 	if ( ret != PAPI_OK )
 		std::cout << "FAIL remove event" << endl; 
@@ -292,5 +298,5 @@ int main (int argc, char *argv[])
 	ret = PAPI_destroy_eventset( &EventSet );
 	if ( ret != PAPI_OK )
 		std::cout << "FAIL destroy" << endl;
-
+*/
 }
