f735cc8d023d778dabf32fb462ac1b83b45a32cf
brunabrasil <up202007191@g.uporto.pt>
2023-05-21 21:40:33 +0100

    simple and rank mode. some fixes

--- assign2/src/QueueHandler.java
+++ assign2/src/QueueHandler.java
@@ -12,15 +12,19 @@ public class QueueHandler implements Runnable {
 
     @Override
     public void run() {
-        long startTime = System.currentTimeMillis();
+        int tries = 0;
+
 
         while (true){
             try {
                 Thread.sleep(1000); // Wait for some time before checking the queue again
+
                 Server.lockPlayersQueue.lock();
                 for (Player player : Server.playersQueue) {
 
-                    System.out.println("|queue " + player.getUsername() + " " + player.getToken().getToken() + " "  + player.isLoggedIn() + " " + player.getRank() + "|");
+                    System.out.println("queue | name: " + player.getUsername() + " | token: " + player.getToken().getToken() + " | logged in: "  + player.isLoggedIn() + " | rank: " + player.getRank() + "|");
+
+                    //renovar token dos players logados na queue
                     if(player.isSocketChannelOpen()) {
                         if (player.getToken() != null && player.getToken().hasExpired() && player.isLoggedIn()) {
 
@@ -29,6 +33,7 @@ public class QueueHandler implements Runnable {
                             Authentication.writeTokenToFile(player.getUsername(), newToken.getToken());
                         }
                     }
+                    //dar logout dos jogadores desconectados e retirar da queue os jogadores nao logados e com token expirado
                     else {
                         player.logout();
                         if (player.getToken() != null && player.getToken().hasExpired()) {
@@ -43,83 +48,86 @@ public class QueueHandler implements Runnable {
                 // Filter the list to get only logged-in players1
                 List<Player> loggedInPlayers = Server.playersQueue.stream().filter(Player::isLoggedIn).collect(Collectors.toList());
 
-                if(loggedInPlayers.size() >= 3){
+                if(loggedInPlayers.size() >= 3) {
 
-                    List<Integer> playerRankDifference = calculateRankDifference(loggedInPlayers);
+                    if (Server.mode == 2) {
+                        List<Integer> playerRankDifference = calculateRankDifference(loggedInPlayers);
 
-                    for(int i = 0; i < playerRankDifference.size(); i++){
-                        // if i have a perfect match (rank difference = 0) -> create a game with those players
-                        if(playerRankDifference.get(i) == 0){
-                            System.out.println("perfect matchhh");
-                            // create a new game thread
-                            List<Player> gamePlayers = new ArrayList<>();
-                            for (int j = 0; j < 3; j++) {
-                                //remove da queue
-                                Server.lockPlayersQueue.lock();
-                                Server.playersQueue.remove(loggedInPlayers.get(i + j));
-                                Server.lockPlayersQueue.unlock();
+                        tries++;
+                        System.out.println("\nnumber of tries to match: " + tries);
 
-                                //adiciona a lista de jogadores para o novo jogo
-                                gamePlayers.add(loggedInPlayers.get(i + j));
+                        for (int i = 0; i < playerRankDifference.size(); i++) {
+                            // if i have a perfect match (rank difference = 0) -> create a game with those players
+                            if (playerRankDifference.get(i) == 0) {
+                                // create a new game thread
+                                List<Player> gamePlayers = new ArrayList<>();
+                                for (int j = 0; j < 3; j++) {
+                                    //remove da queue
+                                    //Server.lockPlayersQueue.lock();
+                                    Server.playersQueue.remove(loggedInPlayers.get(i + j));
+                                    //Server.lockPlayersQueue.unlock();
+
+                                    //adiciona a lista de jogadores para o novo jogo
+                                    gamePlayers.add(loggedInPlayers.get(i + j));
+                                }
+
+                                String gameId = UUID.randomUUID().toString();
+                                Server.executor.submit(new GameThread(gamePlayers, gameId));
+
+                                //recomeçar a contagem de tentativas
+                                tries = 0;
+
+                                break; // so consigo criar um jogo, porque posso tirar 3 jogadores e um deles fazer parte de outra combinação perfeita
                             }
 
-                            String gameId = UUID.randomUUID().toString();
-                            Server.executor.submit(new GameThread(gamePlayers, gameId));
+                            // else, i want to create a game with the players with the smallest rank difference
+                            else if (tries > 2) {
+                                // get the index of the player with the smallest rank difference
+                                int minIndex = playerRankDifference.indexOf(Collections.min(playerRankDifference));
+                                // create a new game thread
+                                List<Player> gamePlayers = new ArrayList<>();
+                                for (int j = 0; j < 3; j++) {
+                                    //remove da queue
+                                    //Server.lockPlayersQueue.lock();
+                                    Server.playersQueue.remove(loggedInPlayers.get(i + j));
+                                    //Server.lockPlayersQueue.unlock();
 
-                            //recomeçar a contagem de tempo
-                            startTime = System.currentTimeMillis();
+                                    gamePlayers.add(loggedInPlayers.get(minIndex + j));
+                                }
 
-                            break; // so consigo criar um jogo, porque posso tirar 3 jogadores e um deles fazer parte de outra combinação perfeita
-                        }
+                                String gameId = UUID.randomUUID().toString();
+                                Server.executor.submit(new GameThread(gamePlayers, gameId));
 
-                        // else, i want to create a game with the players with the smallest rank difference
-                        else if (System.currentTimeMillis() - startTime >= 7000) {
-                            System.out.println("not perfecttt");
-                            // get the index of the player with the smallest rank difference
-                            int minIndex = playerRankDifference.indexOf(Collections.min(playerRankDifference));
-                            // create a new game thread
-                            List<Player> gamePlayers = new ArrayList<>();
-                            for (int j = 0; j < 3; j++) {
-                                //remove da queue
-                                Server.lockPlayersQueue.lock();
-                                Server.playersQueue.remove(loggedInPlayers.get(i + j));
-                                Server.lockPlayersQueue.unlock();
+                                //recomeçar a contagem de tentativas
+                                tries = 0;
 
-                                gamePlayers.add(loggedInPlayers.get(minIndex + j));
+                                break;
                             }
-
-                            String gameId = UUID.randomUUID().toString();
-                            Server.executor.submit(new GameThread(gamePlayers, gameId));
-
-                            //recomeçar a contagem de tempo
-                            startTime = System.currentTimeMillis();
-
-                            break;
                         }
-                        else {
-                            System.out.println("ainda naoo");
+
+                    } else if (Server.mode == 1){
+
+                        // create a new game thread
+                        List<Player> gamePlayers = new ArrayList<>();
+
+                        for (int i = 0; i < 3; i++){
+                            gamePlayers.add(loggedInPlayers.get(i));
+
+                            //Server.lockPlayersQueue.lock();
+                            Server.playersQueue.remove(loggedInPlayers.get(i));
+                            //Server.lockPlayersQueue.unlock();
                         }
+
+                        String gameId = UUID.randomUUID().toString();
+                        Server.executor.submit(new GameThread(gamePlayers, gameId));
                     }
-
                 }
-
-                /*if (Server.playersQueue.size() >= 2) {
-                    // create a new game thread
-                    List<Player> gamePlayers = new ArrayList<>();
-                    for (int i = 0; i < 2; i++) {
-                        gamePlayers.add(Server.playersQueue.poll());
-                    }
-
-                    String gameId = UUID.randomUUID().toString();
-                    Server.executor.submit(new GameThread(gamePlayers, gameId));
-                }*/
 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 Server.lockPlayersQueue.unlock();
             }
-
         }
     }
 
